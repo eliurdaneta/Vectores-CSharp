3628fd751473d584bd0ce1ff2b89872fad9f21d5
eliurdaneta <eliurdaneta@gmail.com>
2020-03-22 21:48:35 -0300

    Ejemplos de Ejercicio uso de Vectores, en este ejercicio encontratran como capturar y mostrar 5 sueldos haciendo uso de un vector

---
+++ VectorSueldo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorSueldo
+{
+    class Program
+    {
+        /// <summary>
+        /// //Este programa tienen la finaldiad de leer y mostras 5 sueldos utilizando vectores
+        /// </summary>
+        /// <param name="args"></param>
+
+        private int[] sueldo;
+        public void CargarDatos()
+        {
+            sueldo = new int[5];
+            string linea;
+            for(int x=0; x<5; x++)
+            {
+                Console.Write("Ingrese sueldo ");
+                linea = Console.ReadLine();
+                sueldo[x] =int.Parse(linea);
+            }
+        }
+
+        public void MostrarDatos()
+        {
+            for(int x=0; x<5; x++)
+            {
+                Console.WriteLine("El sueldo " + x + " es: " + sueldo[x]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program s = new Program();
+            s.CargarDatos();
+            s.MostrarDatos();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VectorSueldo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("VectorSueldo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VectorSueldo")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3e3ec34a-67fc-4025-9848-a7ae247828d3")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
